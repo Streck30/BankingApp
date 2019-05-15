4a098ea5f40f50be19bbbd0e90cd64b4acce7e5e
Streck30 <Streck28@gmail.com>
2019-05-15 16:03:32 -0700

    Working with basics of swing

--- src/bankingapp/ATMGUI.java
+++ src/bankingapp/ATMGUI.java
@@ -15,12 +15,26 @@ import java.awt.event.ActionListener;
  * @author Streck
  */
 public class ATMGUI extends JFrame implements ActionListener {
-  
+  private TextField userName = new TextField();
+  private JPasswordField password = new JPasswordField();
+  private JLabel userNameLabel = new JLabel("username");
+  private JLabel passwordLabel = new JLabel("password");
+  private JPanel panel = new JPanel(new GridLayout(2,1));
+  private JButton login = new JButton("Login");
   public static void main(String [] args){
-    
+    ATMGUI atm = new ATMGUI();
   }
   public ATMGUI(){
-  
+    setSize(300,100);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
+    add(panel, BorderLayout.CENTER);
+    panel.add(userNameLabel);
+    panel.add(userName);
+    panel.add(passwordLabel);
+    panel.add(password);
+    add(login, BorderLayout.PAGE_END);
+    setVisible(true);
   }
   public void actionPerformed(ActionEvent e) {
     
